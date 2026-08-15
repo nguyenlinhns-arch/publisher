1e0e8edc4fa40b9f7a150f919ff492f7cea91c9f
nguyenlinhns-arch <nguyenlinhns@gmail.com>
2026-08-15 21:27:02 +0700

    Load shared institutional site shell

--- assets/site-config.js
+++ assets/site-config.js
@@ -6,3 +6,13 @@ window.HLX_CONFIG={
   GOOGLE_ADS_CALL_CONVERSION_LABEL:'_bbHCP7SqeAcEKn0tog-',
   GOOGLE_ADS_ZALO_CONVERSION_LABEL:'A5g8CM7OuOAcEKn0tog-'
 };
+(function(){
+  var css=document.createElement('link');
+  css.rel='stylesheet';
+  css.href='/assets/official-site.css?v=20260815';
+  document.head.appendChild(css);
+  var shell=document.createElement('script');
+  shell.src='/assets/official-shell.js?v=20260815';
+  shell.defer=true;
+  document.head.appendChild(shell);
+})();
